eb14fdb2a251b970bcd9913d33fe6980ac5f3106
ZiusDH <dhiogomaquine@gmail.com>
2022-11-16 23:00:01 -0400

    tela de login

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tela_de_login/Pages/login.page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kamamy',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
